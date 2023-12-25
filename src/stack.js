d4c463340d9b8e2b9d9b6aab2772e9a85535588e
Xats <xats89@gmail.com>
2023-12-25 16:40:23 +0300

    init: stack

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  constructor() {
+    this.storage = [];
+  }
+  push(element) {
+    return this.storage.push(element)
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    return this.storage.pop()
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    return this.storage[this.storage.length - 1]
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
